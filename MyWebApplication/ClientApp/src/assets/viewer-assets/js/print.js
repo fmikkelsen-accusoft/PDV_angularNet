0e9226d02ce1d5c3a9c8a8daafa69403e4c979ae
Fred Mikkelsen <83080109+fmikkelsen-accusoft@users.noreply.github.com>
2021-04-30 12:17:36 -0400

    First infusion of customized code

---
+++ MyWebApplication/ClientApp/src/assets/viewer-assets/js/print.js
@@ -0,0 +1,10 @@
+// Copyright (C) 1996-2020 Accusoft Corporation
+// See https://github.com/Accusoft/prizmdoc-viewer/blob/master/LICENSE
+
+"use strict";
+var el = document.querySelector('#print');
+
+var options = JSON.parse(el.getAttribute('data-options'));
+options.pages = options.pages.split(',');
+
+PCCViewer.PrintControl(document.body, options);
